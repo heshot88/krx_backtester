f006fd87d4b34861a1ca792fc0a1651c4aa90140
heshot <heshot88@gmail.com>
2024-09-26 13:31:30 +0900

    init 추가

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name='krx_backtester',  # 패키지 이름
     version='0.2.1',  # 패키지 버전
     packages=find_packages(),  # 포함할 패키지 자동 탐색
-    install_requires=['pandas','numpy','asyncio','SQLAlchemy'],
+    install_requires=['pandas','numpy','asyncio','SQLAlchemy','openpyxl'],
     author='heshot88',  # 작성자 이름
     author_email='heshot88@gmail.com',  # 작성자 이메일
     description='A backtesting package for KRX data.',  # 패키지 설명
